ee5ad6d45564251863576e98bfb4f59a003f4f07
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-08-07 18:09:03 +0700

    feat: some file

---
+++ conditional.java
@@ -0,0 +1,40 @@
+public class conditional {
+  int guestCount;
+  int restaurantCapacity;
+  boolean isRestaurantOpen;
+  boolean isConfirmed;
+  
+  public conditional(int count, int capacity, boolean open) {
+    if (count < 1 || count > 8) {
+      System.out.println("Invalid reservation!");
+    }
+    guestCount = count;
+		restaurantCapacity = capacity;
+		isRestaurantOpen = open;
+  }  
+  
+  public void confirmReservation() {
+    if (restaurantCapacity >= guestCount && isRestaurantOpen) {
+      System.out.println("Reservation confirmed");
+      isConfirmed = true;
+    } else {
+      System.out.println("Reservation denied");
+			isConfirmed = false;
+    }
+  }
+  
+  public void informUser() {
+    if (!isConfirmed) {
+      System.out.println("Unable to confirm reservation, please contact restaurant.");
+    } else {
+      System.out.println("Please enjoy your meal!");
+    }
+  }
+  
+  public static void main(String[] args) {
+    conditional partyOfThree = new conditional(3, 12, true);
+    
+    partyOfThree.confirmReservation();
+    partyOfThree.informUser();
+  }
+}
