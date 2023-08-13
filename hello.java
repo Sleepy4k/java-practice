3bd049c43ca3535ecc7b606fcf174697947fab46
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-08-14 00:30:00 +0700

    chore: hello

--- hello.java
+++ hello.java
@@ -3,5 +3,17 @@ public class hello {
     System.out.println("Hello World!");
     System.out.print("My name is ");
     System.out.print("Benjamin4k");
+    System.out.println();
+
+    String name = "Benjamin4k";
+    int age = 20;
+    char grade = 'A';
+
+    /*
+     * %s - String
+     * %d - int
+     * %c - char
+     */
+    System.out.printf("My name is %s, I am %d years old, and I got a %c in my class", name, age, grade);
   }
 }
