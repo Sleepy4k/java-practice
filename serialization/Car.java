b95c17767bb50e7a369008f580f7c90a0c11c86e
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-08-15 22:00:15 +0700

    chore: car serialization course

--- serialization/Car.java
+++ serialization/Car.java
@@ -11,20 +11,22 @@ import java.io.FileNotFoundException;
 public class Car implements Serializable {
   private String name;
   private int year;
+  private transient String color; // transient keyword will not serialize this field
   private static final long serialVersionUID = 1L;
 
-  public Car(String productName, int lauchYear) {
+  public Car(String productName, int lauchYear, String color) {
     this.name = productName;
     this.year = lauchYear;
+    this.color = color;
   }
 
   public String toString(){
-    return String.format("Car make is: %s, Car year is: %d", this.name, this.year);
+    return String.format("Car make is: %s, Car year is: %d, Car color is: %s", this.name, this.year, this.color);
   }
 
   public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException {
-    Car toyota = new Car("Toyota", 2021);
-    Car honda = new Car("Honda", 2020);
+    Car toyota = new Car("Toyota", 2021, "Red");
+    Car honda = new Car("Honda", 2020, "Blue");
 
     FileOutputStream fileOutputStream = new FileOutputStream("./serialization/cars.txt");
     ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
@@ -45,11 +47,11 @@ public class Car implements Serializable {
     boolean isToyotaSameObject = toyotaCopy == toyota;
     boolean isHondaSameObject = toyotaCopy == toyota;
 
-    System.out.println("Toyota (Copy) - "+toyotaCopy);
-    System.out.println("Toyota (Original) - "+toyota);
-    System.out.println("Toyota (Copy) - "+hondaCopy);
-    System.out.println("Toyota (Original) - "+honda);
+    System.out.println("Toyota (Copy) - " + toyotaCopy);
+    System.out.println("Toyota (Original) - " + toyota);
+    System.out.println("Toyota (Copy) - " + hondaCopy);
+    System.out.println("Toyota (Original) - " + honda);
     System.out.println("Is toyota same object: "+ isToyotaSameObject);
-    System.out.println("Is honda same object: "+ isHondaSameObject);
+    System.out.println("Is honda same object: " + isHondaSameObject);
   }
 }
